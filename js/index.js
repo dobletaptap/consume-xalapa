1967259148facc93c0c3b380a67eba114fd0d9a2
Jose Cuevas <josecueram@gmail.com>
2020-03-30 10:17:57 -0600

    added animations to heaader links

--- js/index.js
+++ js/index.js
@@ -6,7 +6,16 @@ const menuToggle = () => {
     menu.classList.toggle('c-header__nav--active')
 }
 
-burgerButton.addEventListener('click', menuToggle)
+burgerButton.addEventListener('click', () => {
+    menu.classList.toggle('c-header__nav--active')
+    links.forEach((link, index) => {
+        if (link.style.animation) {
+            link.style.animation = ''
+        } else {
+            link.style.animation = `fade .5s ease forwards ${index / 7 + .3}s`
+        }
+    })
+})
 
 links.forEach(link => {
     link.addEventListener('click', menuToggle)
